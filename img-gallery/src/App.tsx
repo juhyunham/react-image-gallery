1e1799d570a00e59cd8e9b71fb2d5b771a1289dc
hamju <judev0227@gmail.com>
2022-07-05 01:32:33 +0900

    useRef - input filie

--- img-gallery/src/App.tsx
+++ img-gallery/src/App.tsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ImageBox from "./components/ImageBox";
 
 function App() {
+  const RefFileInput = useRef<HTMLInputElement>(null);
+
   return (
     <div>
       <div className="img-wrapper">
         <p className="text-center">
           이미지가 없습니다. <br /> 이미지를 추가해주세요.
         </p>
+        <input type="file" ref={RefFileInput} className="input-file-box" />
         <div className="image-wrap">
-          <button type="button" className="plus-box">
+          <button type="button" className="plus-box" onClick={() => RefFileInput.current?.click()}>
             +
           </button>
-          <ImageBox src="" />
-          <ImageBox src="" />
-          <ImageBox src="" />
         </div>
       </div>
     </div>
