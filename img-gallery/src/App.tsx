59da2a6395224789547c630c3c20f377401ff9ee
hamju <judev0227@gmail.com>
2022-07-05 01:24:15 +0900

    imageBox 컴포넌트 생성

--- img-gallery/src/App.tsx
+++ img-gallery/src/App.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ImageBox from "./components/ImageBox";
 
 function App() {
   return (
     <div>
-      <div className="img-box-wrap">
+      <div className="img-wrapper">
         <p className="text-center">
           이미지가 없습니다. <br /> 이미지를 추가해주세요.
         </p>
-        <div className="img-box">
+        <div className="image-wrap">
           <button type="button" className="plus-box">
             +
           </button>
+          <ImageBox src="" />
+          <ImageBox src="" />
+          <ImageBox src="" />
         </div>
       </div>
     </div>
---
+++ img-gallery/src/components/ImageBox.tsx
@@ -0,0 +1,9 @@
+function ImageBox(props: { src: string }) {
+  return (
+    <div className="image-box">
+      <img src={props.src} alt =""/>
+    </div>
+  );
+}
+
+export default ImageBox;
